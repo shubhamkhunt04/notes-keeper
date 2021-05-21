e56eedec07c22d462495bb587f891660405d55ab
Shubham Khunt <55044317+shubhamkhunt04@users.noreply.github.com>
2021-05-21 19:28:42 +0530

    github-ribbons added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -57,6 +57,7 @@ const Dashboard = () => {
   return (
     <>
       <AppNavBar />
+      <a href="https://github.com/shubhamkhunt04/notes-keeper"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_white_ffffff.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" /></a>
       <div className={classes.container}>
         <AddNote />
         <Notes />
